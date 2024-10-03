ffb3ca51e775cd5a793ebd777b83a1f82f4bbb6c
Andrea Patricia <andreapatriciazr@gmail.com>
2024-10-03 14:57:16 -0700

    Modificaciones

--- Codigo6.java
+++ Codigo6.java
@@ -1,24 +1,30 @@
-public class codigo6 {
-	  
-	    int[] n = int[20];
+import java.util.Scanner; // importar scanner
 
-	    for (int i = 0; i < 20; i+) {
-	      n[i] = (int)(Math.random() * 381) + 20;
-	      System.print(n[i] + " ");
-	    }
-	    
-	    System.out.printl("\n¿Qué números quiere resaltar? ");
-	    System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): ");
-	    int opcion = Integer.parseInt(System.console().readLine();
+public class Codigo6 {
 
-	    int multiplo = (opcion == 1) : 5 ? 7;
+    public static void main(String[] args) { // agregar main
+        int[] n = new int[20]; // inicio arreglo, corregir
 
-	    foreach (char e : n) {
-	      if (e % multiplo == 0) {
-	        System.out.print("[" + e + "] ");
-	       else {
-	        System.in.print(e + " ");
-	      }
-	    }
-	  
-	}
+        for (int i=0; i<20; i++) { // completar la actualizacion para la incrementacion al hacer la iteracion
+            n[i] = (int)(Math.random()*381) + 20; // obtener numero random
+            System.out.print(n[i] + " "); // completar la funcion de impresion de numero
+        } // for
+
+        System.out.println("\n¿Qué números quiere resaltar?"); // completar la funcion de impresion
+        System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): ");
+        Scanner scanner = new Scanner(System.in); // agregar scanner
+        int opcion = Integer.parseInt(scanner.nextLine()); 
+
+        int multiplo = (opcion == 1) ? 5 : 7; // corregir operador ternario
+
+        for (int e : n) {
+            if (e % multiplo == 0) {
+                System.out.print("[" + e + "] "); // imprimir numero con resalte
+            } else {
+                System.out.print(e + " "); // imprimir numero
+            } // for else
+        } // for each
+        
+        scanner.close(); // cerrar scanner
+    } // main
+} // class Codigo6
